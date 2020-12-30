9e23b4c8f6a915c5ec1f2cf464c3c1279e2f949b
Mohamed Bdioui <mohamed.bdioui@esprit.tn>
2020-12-30 19:00:23 +0100

    stock exception

--- src/main/java/bookstore/exception/UserException.java
+++ src/main/java/bookstore/exception/UserException.java
@@ -2,6 +2,11 @@ package bookstore.exception;
 
 public class UserException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public UserException() {
 		super();
 	}
