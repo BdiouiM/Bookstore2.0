59102c1c72a22bb482514a6e6e7c0679d87220f5
Mohamed Bdioui <mohamed.bdioui@esprit.tn>
2021-01-16 09:42:17 +0100

    new commit

--- src/main/java/bookstore/controll/ReclamationController.java
+++ src/main/java/bookstore/controll/ReclamationController.java
@@ -1,11 +1,13 @@
 package bookstore.controll;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,29 +27,31 @@ public class ReclamationController {
 	@GetMapping("/reclamation-welcome")
 	public String start(){return "Weclome to reclamation";}
 	
-	@RequestMapping("/reclamation-list")
+	@GetMapping("/reclamation-list")
     public Iterable<Reclamation> getAll() {
 		
 		return reclamationAdminService.findAll();
 	}
-	/* @RequestMapping(value = "/envoyer", method = RequestMethod.POST)
-	    public void envoyerReclamation(@RequestBody Reclamation r) {
+	@PostMapping(/*value=*/"/reclamation-add"/*, method = RequestMethod.POST*/)
+	    public  ResponseEntity<String> envoyerReclamation(@RequestBody Reclamation r) {
 	         try {
-				reclamationClientService.envoyerReclamation(r);
+				 reclamationClientService.envoyerReclamation(r);
+				 return  new ResponseEntity("reclamation envoyée", HttpStatus.OK);
 			} catch (ReclamationException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
+	         return  new ResponseEntity("error add", HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
-	@RequestMapping(value = "/annulerRclient", method = RequestMethod.POST)
+	
+	@PostMapping(/*value = */"/reclamation-delete"/*, method = RequestMethod.POST*/)
 	public ResponseEntity<String> annulerReclamation(@RequestBody Reclamation r){
 		try {
-			reclamationClientService.annulerReclamationsClient(r);
+	            reclamationClientService.annulerReclamationsClient(r);
+	            return  new ResponseEntity("reclamation annulée", HttpStatus.OK);
 		} catch (ReclamationException e) {
 			e.printStackTrace();
 		}
-		return  new ResponseEntity("reclamation annulée", HttpStatus.OK);
-	}*/
+		return  new ResponseEntity("error delete", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 }
