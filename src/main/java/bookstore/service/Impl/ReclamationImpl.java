f5295289f1ddbe16e92f08c879f7f233ea715a2a
Mohamed Bdioui <mohamed.bdioui@esprit.tn>
2021-01-17 19:24:30 +0100

    new commit

---
+++ src/main/java/bookstore/service/Impl/ReclamationImpl.java
@@ -0,0 +1,150 @@
+package bookstore.service.Impl;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import bookstore.entities.Reclamation;
+import bookstore.entities.StatutReclamation;
+import bookstore.entities.TypeReclamation;
+import bookstore.exception.ReclamationException;
+import bookstore.repository.ReclamationRepository;
+import bookstore.service.ReclamationAdminService;
+import bookstore.service.ReclamationClientService;
+
+public class ReclamationImpl implements ReclamationAdminService,ReclamationClientService{
+	@Autowired
+	ReclamationRepository reclamationRepository;
+	private static final Logger L = LogManager.getLogger(ReclamationImpl.class);
+	public ReclamationImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	// GESTION RECLAMATION -MOHAMED BDIOUI-
+		@Override
+		public Iterable<Reclamation> findAll() {
+			return reclamationRepository.findAll();
+		}	
+
+		@Override
+		public boolean existeReclamation(Reclamation r) {
+			//long idR = r.getId();
+			List<Reclamation> reclamation= reclamationRepository.afficherReclamationsSelonID(r.getId());
+			if(reclamation.isEmpty())
+			return false;
+			else
+		    return true;
+		}
+		@Override
+		public void envoyerReclamation(Reclamation r) throws ReclamationException {
+			if(existeReclamation(r))
+				throw (new ReclamationException("reclamation existe"));
+			else 
+				 {reclamationRepository.save(r);
+				  L.info("reclamation envoyée");
+				 }
+		}
+
+		@Override
+		public void modifierReclamation(Reclamation r,Date date,String description,TypeReclamation type) throws ReclamationException {
+			if(!existeReclamation(r))
+				throw (new ReclamationException("reclamation n'existe pas"));
+			else
+			{
+				long idR=r.getId();
+				r.setType(type);
+				r.setDescription(description);
+				r.setDateReclamation(date);
+				r=reclamationRepository.modifierReclamation(idR, r.getDateReclamation(), r.getDescription(), r.getType());
+				reclamationRepository.save(r);
+				L.info("reclamation modifiée");
+			}
+		}
+		
+		@Override
+		public boolean validerReclamations(Reclamation r) throws ReclamationException {
+			if(!existeReclamation(r))
+				throw (new ReclamationException("reclamation n'existe pas"));
+			else
+			{
+				long idR=r.getId();
+				r.setStatutReclamation(StatutReclamation.VALIDEE);
+				StatutReclamation statutR=r.getStatutReclamation();
+				r=reclamationRepository.changerReclamationSelonStatut(statutR, idR);
+				reclamationRepository.save(r);
+				L.info("Reclamation annulée");
+				return true;
+			}
+		}
+		
+		@Override
+		public boolean annulerReclamation(Reclamation r) throws ReclamationException {
+			if(!existeReclamation(r))
+				throw (new ReclamationException("reclamation n'existe pas"));
+			else
+			{
+				long idR=r.getId();
+				r.setStatutReclamation(StatutReclamation.ANNULEE);
+				StatutReclamation statutR=r.getStatutReclamation();
+				r=reclamationRepository.changerReclamationSelonStatut(statutR, idR);
+				reclamationRepository.save(r);
+				L.info("Reclamation annulée");
+				return true;
+			}
+		}
+
+		@Override
+		public List<Reclamation> ListReclamations() {
+			List<Reclamation> reclamations= (List<Reclamation>) reclamationRepository.findAll();
+			for(Reclamation reclamation:reclamations){
+				L.info("reclamation +++ :" +reclamation);
+			}
+			return reclamations;
+		}
+
+		@Override
+		public void annulerReclamationsClient(Reclamation r) throws ReclamationException {
+			if(!existeReclamation(r))
+				throw (new ReclamationException("reclamation n'existe pas"));
+			else
+			{
+			long idR=r.getId();
+			reclamationRepository.deleteById(idR);
+			L.info("reclamation supprimée");
+			}
+		}
+
+		
+
+		@Override
+		public boolean TraiterReclamation(Reclamation r) throws ReclamationException {
+			return annulerReclamation(r)||validerReclamations(r);
+			}
+			
+
+		@Override
+		public int NombreReclamations() {
+			return reclamationRepository.nombreReclamations();
+		}
+
+		@Override
+		public void AfficherReclamations(List<Reclamation> reclamations) {
+			reclamations=ListReclamations();
+			for (Reclamation reclamation:reclamations){
+				System.out.println("reclamation out: "+reclamation);
+				L.info("reclamation : "+reclamation);
+			}
+		}
+
+		@Override
+		public Set<Reclamation> afficherReclamation(Reclamation r) throws ReclamationException {
+			List<Reclamation> lreclamation=reclamationRepository.afficherReclamationsSelonID(r.getId());
+			Set<Reclamation> Sreclamation=lreclamation.stream().collect(Collectors.toSet());
+			return Sreclamation;
+		}
+		
+}
---
+++ src/test/java/bookstore/ReclamationTests.java
@@ -0,0 +1,124 @@
+package bookstore;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sun.el.parser.ParseException;
+
+import bookstore.entities.Reclamation;
+import bookstore.entities.TypeReclamation;
+import bookstore.exception.ReclamationException;
+import bookstore.service.ReclamationAdminService;
+import bookstore.service.ReclamationClientService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReclamationTests {
+	@Autowired
+	ReclamationAdminService reclamationAdminService;
+	@Autowired
+	ReclamationClientService reclamationClientService;
+	// TEST ADMIN RECLAMATION -MOHAMED BDIOUI-
+		@Test
+		public void testfindAll() {
+	    	reclamationAdminService.findAll();
+	    }
+	    @Test
+		public void testNombreReclamation() {
+	    	 reclamationAdminService.NombreReclamations();
+	    }
+	    @Test
+		public void testListReclamations() throws ParseException {
+	    	reclamationAdminService.ListReclamations();
+	    }
+	    @Test
+		public void testAnnulerReclamation() throws ParseException {
+	    	Reclamation r = new Reclamation();
+	    	try {
+	    		Assert.assertFalse(reclamationAdminService.annulerReclamation(r));
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+			}
+	    }
+	    @Test
+		public void testValiderReclamation() throws ReclamationException {
+	    	Reclamation r = new Reclamation();
+	    	try {
+	    		Assert.assertFalse(reclamationAdminService.validerReclamations(r));
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+			}  
+	    }
+	    @Test
+		public void testTraiterReclamation() throws ReclamationException {
+	    	Reclamation r = new Reclamation();
+	    	try {
+	    		Assert.assertFalse(reclamationAdminService.TraiterReclamation(r));
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+			}  
+	    }
+	    @Test
+		public void testAfficherReclamations() throws ParseException {
+	    	List<Reclamation> reclamations=new ArrayList<Reclamation>();
+	    	reclamations=reclamationAdminService.ListReclamations();
+	    	reclamationAdminService.AfficherReclamations(reclamations);
+	    }
+	    @Test
+	   	public void testAfficherReclamation() throws ParseException {
+	       	Reclamation r=new Reclamation();
+	       	try {
+				Set<Reclamation> s =reclamationAdminService.afficherReclamation(r);
+				Set<Reclamation> expected = new HashSet<Reclamation>();
+				Assert.assertEquals(expected,s);
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+				//((org.apache.logging.log4j.Logger) L).debug("error afficher reclamation");
+			}
+	       }
+	    @Test
+	   	public void testExisteReclamation() throws ParseException {
+	    	Reclamation r= new Reclamation();
+	    	reclamationAdminService.existeReclamation(r);
+	    }
+
+	    // TEST CLIENT RECLAMATION -MOHAMED BDIOUI-
+	    @Test
+	   	public void testEnvoyerReclamation() throws ParseException {
+	    	Reclamation r=new Reclamation();
+	    	try {
+				reclamationClientService.envoyerReclamation(r);
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+				//((org.apache.logging.log4j.Logger) L).debug("error envoyer reclamation");
+			}
+	    }
+	    @Test
+	   	public void testModifierReclamation() throws ParseException {
+	    	Reclamation r=new Reclamation();
+	    	r.setType(TypeReclamation.Level1);
+	    	r.setDescription("description test");
+	    	r.setDateReclamation(null);
+	    	try {
+				reclamationClientService.modifierReclamation(r, r.getDateReclamation(), r.getDescription(), r.getType());
+			} catch (ReclamationException e) {
+				e.printStackTrace();
+			}
+	    }
+	    @Test
+	   	public void testExisteReclamationClient() throws ParseException {
+	    	boolean expected =false;
+	    	Reclamation r = new Reclamation();
+	    	Assert.assertEquals(expected,reclamationClientService.existeReclamation(r));
+	    }
+
+}
